696515d0521621520b64016b219d0da8d2666e90
Marcin Lewandowski <marcin@ravendb.net>
2024-04-05 21:13:42 +0200

    using unicore to enforce rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "plugin:unicorn/recommended"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
@@ -14,12 +15,24 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "@typescript-eslint"
+        "@typescript-eslint",
+        "unicorn"
     ],
     "rules": {
+        "no-console": "warn",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-inferrable-types": "off",
         "@typescript-eslint/no-unused-vars": "off",
-        "no-console": "warn",
+        "unicorn/prefer-node-protocol": "error",
+        "unicorn/prevent-abbreviations": "off",
+        "unicorn/filename-case": "off",
+        "unicorn/no-null": "off",
+        "unicorn/catch-error-name": "off",
+        "unicorn/no-array-push-push": "off",
+        "unicorn/no-lonely-if": "off",
+        "unicorn/explicit-length-check": "off",
+        "unicorn/numeric-separators-style": "off",
+        "unicorn/no-await-expression-member": "off",
+        "unicorn/no-zero-fractions": "off"
     }
 }
