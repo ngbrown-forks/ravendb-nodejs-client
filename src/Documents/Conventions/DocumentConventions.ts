085cba129834227796558e67ce11ec46d2960514
Marcin Lewandowski <marcin@ravendb.net>
2024-08-05 11:59:47 +0200

    inlined pluralize

--- src/Documents/Conventions/DocumentConventions.ts
+++ src/Documents/Conventions/DocumentConventions.ts
@@ -7,7 +7,6 @@ import {
     ObjectLiteralDescriptor,
     ClassConstructor, EntityConstructor, Field
 } from "../../Types/index.js";
-import { plural } from "pluralize";
 import { ClientConfiguration } from "../Operations/Configuration/ClientConfiguration.js";
 import { ReadBalanceBehavior } from "../../Http/ReadBalanceBehavior.js";
 import { throwError } from "../../Exceptions/index.js";
@@ -19,6 +18,7 @@ import { LoadBalanceBehavior } from "../../Http/LoadBalanceBehavior.js";
 import { BulkInsertConventions } from "./BulkInsertConventions.js";
 import { InMemoryDocumentSessionOperations } from "../Session/InMemoryDocumentSessionOperations.js";
 import { ShardingConventions } from "./ShardingConventions.js";
+import { plural } from "../../ext/pluralize/pluralize.js";
 
 export type IdConvention = (databaseName: string, entity: object) => Promise<string>;
 export type IValueForQueryConverter<T> =
